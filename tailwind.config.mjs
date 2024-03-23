2a5d1b9d8253c9db07f0e1600476c9d15ec3a7c7
shansally <sln6124639@gmail.com>
2024-03-23 19:26:52 +0800

    init

---
+++ tailwind.config.mjs
@@ -0,0 +1,35 @@
+import plugin from "tailwindcss/plugin";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        Mycolor: {
+          green: "#3F5D45",
+          lightgreen: "#EAF0ED",
+          blue: "#EAF0ED",
+          yellow: "#FFE180",
+          white: "#FFFFFF",
+        },
+      },
+    },
+  },
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        // https://developer.mozilla.org/en-US/docs/Web/CSS/writing-mode
+        ".horizontal-writing-tb": { "writing-mode": "horizontal-tb" },
+        ".vertical-writing-rl": { "writing-mode": "vertical-rl" },
+        ".vertical-writing-lr": { "writing-mode": "vertical-lr" },
+        // https://developer.mozilla.org/en-US/docs/Web/CSS/text-orientation
+        ".orientation-mixed": { "text-orientation": "mixed" },
+        ".orientation-upright": { "text-orientation": "upright" },
+        ".orientation-sideways-right": { "text-orientation": "sideways-right" },
+        ".orientation-sideways": { "text-orientation": "sideways" },
+        ".orientation-glyph": { "text-orientation": "use-glyph-orientation" },
+      });
+    }),
+  ],
+};
